8884ddb324a65793c056d0bd1e29e5dc6f39e78e
Alice <herrada.aliciab@gmail.com>
2012-03-19 17:45:21 -0300

    first commit

---
+++ ejer3-4/index.js
@@ -0,0 +1,21 @@
+/* 
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+$(document).ready(function(){
+    
+                      // 3.Show an alert when page has finished loading.
+                      
+                      alert("The page is already load");
+                      
+                      // 4.b. Put the cursor inside it right after alert closes
+                      
+                      document.getElementById("alias").focus();
+                      
+              });
+
+
+
+
